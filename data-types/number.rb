95bbd0b8d8c1bdc1f5e7e48bde0191a8ea709e52
fhyfr <fhaya.firman@gmail.com>
2022-12-21 15:31:56 +0700

    ruby-basic: number data types

---
+++ data-types/number.rb
@@ -0,0 +1,18 @@
+# data types number
+
+# initialize new variables
+# integer data type
+myAge, myBrotherAge = 23, 19
+
+# get age difference
+ageDifference = myAge - myBrotherAge
+
+# we can use #{} to concat string with other data types
+puts "The difference between me and my brother age is #{ageDifference} years."
+
+
+# float data type
+totalScores = 80.5 + 81.25 + 75.0 + 70
+gpa = totalScores / 4
+
+puts "My GPA is #{gpa}"
