8ca23b8ea46c88238f6a8fc1c52ffb2ba914c787
fhyfr <fhaya.firman@gmail.com>
2022-12-21 15:04:16 +0700

    ruby-basic: string data types

---
+++ data-types/string.rb
@@ -0,0 +1,24 @@
+puts "Hello World!"
+
+# this is comments and not executable
+puts "this is string data types on ruby"
+
+# define new variables on ruby
+# variable on ruby is low typing that means
+# you can insert any data types on one variables
+name = "Firman"
+puts name
+
+# you can override variables with different data types
+# for example we override string with integer
+name = 28
+puts name
+
+# single quotation inside string
+greetings = "Hello World! You are 'amazing!'"
+puts greetings
+
+# concat strings
+firstName, lastName = "Firman", "Sah"
+puts firstName + " " + lastName
+
