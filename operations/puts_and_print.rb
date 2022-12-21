6cf85c980b4af9bfa7968e337a9c36e3afa2870b
fhyfr <fhaya.firman@gmail.com>
2022-12-21 15:40:25 +0700

    ruby-basic: the difference between puts and print

---
+++ operations/puts_and_print.rb
@@ -0,0 +1,19 @@
+# the difference between puts and print is
+# puts automatically prints a new line after the end of a sentence
+# print will prints the sentence in single line
+
+
+# example of puts
+firstName, lastName = "Firman", "Sah"
+
+# the output is
+# Firman
+# Sah
+puts firstName
+puts lastName
+
+# example of print
+# the output is
+# FirmanSah
+print firstName
+print lastName
