673d5994d433ef470ba39497bb316fad0a7c8ab9
fhyfr <fhaya.firman@gmail.com>
2022-12-22 14:49:27 +0700

    ruby-basic: access level on class

--- oop/class.rb
+++ oop/class.rb
@@ -52,3 +52,51 @@ end
 firman = Firman.new("Footbal")
 firman.set_name_and_age("Firmansah", 23)
 firman.get_biodata()
+
+# access level in class
+# public, protected, private
+
+# public can be access outside class
+# private cannot be access outside class but only inside the class
+# protected only can be access inside the class and child class
+class Animal
+  def initialize(name)
+    @name = name
+  end
+
+  # public method
+  def get_name()
+    puts "Animal name: #{@name}"
+  end
+
+  # protected method
+  def update_name(name)
+    @name = name
+  end
+
+  protected :update_name
+end
+
+tiger = Animal.new("Tiger")
+# tiger.update_name("Bird") # it will be error because method is protected
+tiger.get_name()
+
+class Dolpin < Animal
+  # private method
+  def set_number_of_legs(legs)
+    @legs = legs
+    puts "Legs: #{legs}"
+  end
+
+  def get_full_details()
+    update_name(@name)
+    puts "Name: #{@name}, Legs: #{@legs}"
+  end
+
+  private :set_number_of_legs
+end
+
+dolpin = Dolpin.new("Dolpin")
+# dolpin.set_number_of_legs(2) # it will be error because method is private
+dolpin.get_full_details()
+
